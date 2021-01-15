95b11a94e4a7a9bc7b3d948e0bc8a9da87c1d490
manuelvargastapia <manuelvargastapia@gmail.com>
2021-01-15 19:38:37 -0300

    create IAuthFacade

---
+++ lib/domain/authentication/i_auth_facade.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/foundation.dart';
+
+import 'package:dartz/dartz.dart';
+
+import 'package:highlights/domain/authentication/auth_failure.dart';
+import 'package:highlights/domain/authentication/value_objects.dart';
+
+/// Interface to simplify use of `FirebaseAuth` and `GoogleSignIn` classes.
+///
+/// This insterface will be implemented as part of the Infrastructure layer.
+abstract class IAuthFacade {
+  Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  });
+
+  Future<Either<AuthFailure, Unit>> signInWithEmailAndPassword({
+    @required EmailAddress emailAddress,
+    @required Password password,
+  });
+
+  Future<Either<AuthFailure, Unit>> signInWithGoogle();
+}
