477914a1d3e3213e23b0fe5348d6a8e92c318aca
manuelvargastapia <manuelvargastapia@gmail.com>
2021-01-16 17:42:41 -0300

    add getOrCrash() helper function

--- lib/domain/core/value_objects.dart
+++ lib/domain/core/value_objects.dart
@@ -2,6 +2,7 @@ import 'package:flutter/foundation.dart';
 
 import 'package:dartz/dartz.dart';
 
+import 'package:highlights/domain/core/errors.dart';
 import 'package:highlights/domain/core/failures.dart';
 
 @immutable
@@ -10,6 +11,12 @@ abstract class ValueObject<T> {
 
   Either<ValueFailure<T>, T> get value;
   bool isValid() => value.isRight();
+
+  /// Folds the [ValueObject] to return it or throws [UnexpectedValueError]
+  /// containing the [ValueFailure]
+  T getOrCrash() {
+    return value.fold((failure) => throw UnexpectedValueError(failure), id);
+  }
 
   @override
   bool operator ==(Object o) {
