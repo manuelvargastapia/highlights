72b26a468e40ba5c012aebe9a76739ea09d3566c
manuelvargastapia <manuelvargastapia@gmail.com>
2021-01-18 10:26:28 -0300

    create AuthProviderManager helper

---
+++ lib/infrastructure/authentication/core/auth_provider_manager.dart
@@ -0,0 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+/// Wrapper class for OAuth credentials for Google sign in
+///
+/// The original purpose of this class was to make it possible
+/// to write unit testing that includes static method calls. Concretely,
+/// `GoogleAuthProvider.credential()` couldn't be mocked, so test couldn't
+/// be passed
+class AuthProviderManager {
+  const AuthProviderManager();
+
+  OAuthCredential getGoogleOAuthCredential(
+    GoogleSignInAuthentication authentication,
+  ) {
+    return GoogleAuthProvider.credential(
+      idToken: authentication.idToken,
+      accessToken: authentication.accessToken,
+    );
+  }
+}
