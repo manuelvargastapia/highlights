10d719ab414350f32cebcadd7d952af57b13f41a
manuelvargastapia <manuelvargastapia@gmail.com>
2021-01-25 18:23:17 -0300

    reallocate file and mark as injectable

--- lib/infrastructure/authentication/auth_provider_manager.dart
+++ lib/infrastructure/authentication/auth_provider_manager.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:injectable/injectable.dart';
 
 /// Wrapper class for OAuth credentials for Google sign in
 ///
@@ -7,6 +8,7 @@ import 'package:google_sign_in/google_sign_in.dart';
 /// to write unit testing that includes static method calls. Concretely,
 /// `GoogleAuthProvider.credential()` couldn't be mocked, so test couldn't
 /// be passed
+@lazySingleton
 class AuthProviderManager {
   const AuthProviderManager();
 
