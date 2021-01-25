8f91588ae52b7d1ed84d40b11e0994bdc4720f73
manuelvargastapia <manuelvargastapia@gmail.com>
2021-01-25 18:22:13 -0300

    autogenerated code for DI

---
+++ lib/injection.config.dart
@@ -0,0 +1,40 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// InjectableConfigGenerator
+// **************************************************************************
+
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:get_it/get_it.dart';
+import 'package:injectable/injectable.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+import 'infrastructure/authentication/auth_provider_manager.dart';
+import 'infrastructure/authentication/firebase_auth_facade.dart';
+import 'infrastructure/core/firebase_injectable_module.dart';
+import 'domain/authentication/i_auth_facade.dart';
+import 'application/authentication/sign_in_form/sign_in_form_bloc.dart';
+
+/// adds generated dependencies
+/// to the provided [GetIt] instance
+
+GetIt $initGetIt(
+  GetIt get, {
+  String environment,
+  EnvironmentFilter environmentFilter,
+}) {
+  final gh = GetItHelper(get, environment, environmentFilter);
+  final firebaseInjectableModule = _$FirebaseInjectableModule();
+  gh.lazySingleton<AuthProviderManager>(() => AuthProviderManager());
+  gh.lazySingleton<FirebaseAuth>(() => firebaseInjectableModule.firebaseAuth);
+  gh.lazySingleton<GoogleSignIn>(() => firebaseInjectableModule.googleSignIn);
+  gh.lazySingleton<IAuthFacade>(() => FirebaseAuthFacade(
+        get<FirebaseAuth>(),
+        get<GoogleSignIn>(),
+        get<AuthProviderManager>(),
+      ));
+  gh.factory<SignInFormBloc>(() => SignInFormBloc(get<IAuthFacade>()));
+  return get;
+}
+
+class _$FirebaseInjectableModule extends FirebaseInjectableModule {}
