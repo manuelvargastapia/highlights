eb3c841339f1e1033f5c1253130e75da673267bd
manuelvargastapia <manuelvargastapia@gmail.com>
2021-01-25 18:23:41 -0300

    create FirebaseInjectableModule to inject 3rd party classes as dependencies

---
+++ lib/infrastructure/core/firebase_injectable_module.dart
@@ -0,0 +1,16 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:injectable/injectable.dart';
+
+/// Module to inject 3rd party dependencies
+///
+/// It keeps any Firebase related 3rd party dependency that can't be annotated
+/// directly, as they aren't part of this project
+@module
+abstract class FirebaseInjectableModule {
+  @lazySingleton
+  GoogleSignIn get googleSignIn => GoogleSignIn();
+
+  @lazySingleton
+  FirebaseAuth get firebaseAuth => FirebaseAuth.instance;
+}
