1a9c514f4b08fbe933d792cf4840b36b66676967
manuelvargastapia <manuelvargastapia@gmail.com>
2021-01-27 00:22:17 -0300

    create User entity

---
+++ lib/domain/authentication/user.dart
@@ -0,0 +1,9 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:highlights/domain/core/value_objects.dart';
+
+part 'user.freezed.dart';
+
+@freezed
+abstract class User with _$User {
+  const factory User({@required UniqueId id}) = _User;
+}
---
+++ lib/domain/authentication/user.freezed.dart
@@ -0,0 +1,126 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies
+
+part of 'user.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+/// @nodoc
+class _$UserTearOff {
+  const _$UserTearOff();
+
+// ignore: unused_element
+  _User call({@required UniqueId id}) {
+    return _User(
+      id: id,
+    );
+  }
+}
+
+/// @nodoc
+// ignore: unused_element
+const $User = _$UserTearOff();
+
+/// @nodoc
+mixin _$User {
+  UniqueId get id;
+
+  @JsonKey(ignore: true)
+  $UserCopyWith<User> get copyWith;
+}
+
+/// @nodoc
+abstract class $UserCopyWith<$Res> {
+  factory $UserCopyWith(User value, $Res Function(User) then) =
+      _$UserCopyWithImpl<$Res>;
+  $Res call({UniqueId id});
+}
+
+/// @nodoc
+class _$UserCopyWithImpl<$Res> implements $UserCopyWith<$Res> {
+  _$UserCopyWithImpl(this._value, this._then);
+
+  final User _value;
+  // ignore: unused_field
+  final $Res Function(User) _then;
+
+  @override
+  $Res call({
+    Object id = freezed,
+  }) {
+    return _then(_value.copyWith(
+      id: id == freezed ? _value.id : id as UniqueId,
+    ));
+  }
+}
+
+/// @nodoc
+abstract class _$UserCopyWith<$Res> implements $UserCopyWith<$Res> {
+  factory _$UserCopyWith(_User value, $Res Function(_User) then) =
+      __$UserCopyWithImpl<$Res>;
+  @override
+  $Res call({UniqueId id});
+}
+
+/// @nodoc
+class __$UserCopyWithImpl<$Res> extends _$UserCopyWithImpl<$Res>
+    implements _$UserCopyWith<$Res> {
+  __$UserCopyWithImpl(_User _value, $Res Function(_User) _then)
+      : super(_value, (v) => _then(v as _User));
+
+  @override
+  _User get _value => super._value as _User;
+
+  @override
+  $Res call({
+    Object id = freezed,
+  }) {
+    return _then(_User(
+      id: id == freezed ? _value.id : id as UniqueId,
+    ));
+  }
+}
+
+/// @nodoc
+class _$_User implements _User {
+  const _$_User({@required this.id}) : assert(id != null);
+
+  @override
+  final UniqueId id;
+
+  @override
+  String toString() {
+    return 'User(id: $id)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _User &&
+            (identical(other.id, id) ||
+                const DeepCollectionEquality().equals(other.id, id)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^ const DeepCollectionEquality().hash(id);
+
+  @JsonKey(ignore: true)
+  @override
+  _$UserCopyWith<_User> get copyWith =>
+      __$UserCopyWithImpl<_User>(this, _$identity);
+}
+
+abstract class _User implements User {
+  const factory _User({@required UniqueId id}) = _$_User;
+
+  @override
+  UniqueId get id;
+  @override
+  @JsonKey(ignore: true)
+  _$UserCopyWith<_User> get copyWith;
+}
