05d157eaa57d28fa704f6906b44eeb6cbf96b25d
manuelvargastapia <manuelvargastapia@gmail.com>
2021-01-27 00:22:47 -0300

    add getSignedInUser and signOut to IAuthFacade

--- lib/domain/authentication/i_auth_facade.dart
+++ lib/domain/authentication/i_auth_facade.dart
@@ -3,12 +3,15 @@ import 'package:flutter/foundation.dart';
 import 'package:dartz/dartz.dart';
 
 import 'package:highlights/domain/authentication/auth_failure.dart';
+import 'package:highlights/domain/authentication/user.dart';
 import 'package:highlights/domain/authentication/value_objects.dart';
 
 /// Interface to simplify use of `FirebaseAuth` and `GoogleSignIn` classes.
 ///
 /// This insterface will be implemented as part of the Infrastructure layer.
 abstract class IAuthFacade {
+  Option<User> getSignedInUser();
+
   Future<Either<AuthFailure, Unit>> registerWithEmailAndPassword({
     @required EmailAddress emailAddress,
     @required Password password,
@@ -20,4 +23,6 @@ abstract class IAuthFacade {
   });
 
   Future<Either<AuthFailure, Unit>> signInWithGoogle();
+
+  Future<void> signOut();
 }
