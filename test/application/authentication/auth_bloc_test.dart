8bb86ec8053085bc0d914c9244bb4dd06fe841c6
manuelvargastapia <manuelvargastapia@gmail.com>
2021-01-29 19:23:38 -0300

    test AuthBloc

---
+++ test/application/authentication/auth_bloc_test.dart
@@ -0,0 +1,61 @@
+import 'package:dartz/dartz.dart';
+import 'package:bloc_test/bloc_test.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+
+import 'package:highlights/domain/authentication/user.dart';
+import 'package:highlights/domain/authentication/i_auth_facade.dart';
+import 'package:highlights/application/authentication/auth_bloc.dart';
+
+class MockAuthFacade extends Mock implements IAuthFacade {}
+
+class MockUser extends Mock implements User {}
+
+void main() {
+  MockAuthFacade mockAuthFacade;
+  MockUser mockUser;
+
+  setUp(() {
+    mockAuthFacade = MockAuthFacade();
+    mockUser = MockUser();
+  });
+
+  group('AuthCheckRequested', () {
+    blocTest(
+      "\nGiven user authenticated\nWhen AuthCheckRequested ocurrs\nThen Authenticated is emitted",
+      build: () {
+        when(mockAuthFacade.getSignedInUser()).thenReturn(Some(mockUser));
+        return AuthBloc(mockAuthFacade);
+      },
+      act: (bloc) => bloc.add(const AuthEvent.authCheckRequested()),
+      expect: [const Authenticated()],
+      verify: (_) {
+        verify(mockAuthFacade.getSignedInUser()).called(1);
+      },
+    );
+
+    blocTest(
+      "\nGiven user unauthenticated\nWhen AuthCheckRequested ocurrs\nThen Unauthenticated is emitted",
+      build: () {
+        when(mockAuthFacade.getSignedInUser()).thenReturn(const None());
+        return AuthBloc(mockAuthFacade);
+      },
+      act: (bloc) => bloc.add(const AuthEvent.authCheckRequested()),
+      expect: [const Unauthenticated()],
+      verify: (_) {
+        verify(mockAuthFacade.getSignedInUser()).called(1);
+      },
+    );
+  });
+  group('SignedOut', () {
+    blocTest(
+      "\nGiven user authenticated\nWhen SignedOut ocurrs\nThen Unauthenticated is emitted",
+      build: () => AuthBloc(mockAuthFacade),
+      act: (bloc) => bloc.add(const AuthEvent.signedOut()),
+      expect: [const Unauthenticated()],
+      verify: (_) {
+        verify(mockAuthFacade.signOut()).called(1);
+      },
+    );
+  });
+}
