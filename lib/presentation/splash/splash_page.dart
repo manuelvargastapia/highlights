363da5130c2ae4bc5ef4fabcf8e7a306241d3d8c
manuelvargastapia <manuelvargastapia@gmail.com>
2021-01-29 18:50:59 -0300

    create SplashPage to handle initial validation and routing

---
+++ lib/presentation/splash/splash_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:auto_route/auto_route.dart';
+
+import 'package:highlights/application/authentication/auth_bloc.dart';
+import 'package:highlights/presentation/routes/router.gr.dart';
+
+/// Initial page responsible for authetication validations for routing
+class SplashPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return BlocListener<AuthBloc, AuthState>(
+      listener: (context, state) {
+        state.map(
+          initial: (_) {},
+          authenticated: (_) {
+            // TODO: remove print and navigate to notes page
+            print("AUTHENTICATED");
+          },
+          unauthenticated: (_) =>
+              ExtendedNavigator.of(context).replace(Routes.signInPage),
+        );
+      },
+      child: const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      ),
+    );
+  }
+}
