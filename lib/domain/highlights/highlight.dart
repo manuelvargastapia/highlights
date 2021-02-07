1bcdcaf4d203ec0609b9aade299564162bf9fd5e
manuelvargastapia <manuelvargastapia@gmail.com>
2021-02-07 14:51:44 -0300

    create Highlight entity

---
+++ lib/domain/highlights/highlight.dart
@@ -0,0 +1,56 @@
+import 'package:dartz/dartz.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+import 'package:highlights/domain/core/failures.dart';
+
+import 'package:highlights/domain/core/value_objects.dart';
+import 'package:highlights/domain/highlights/quote_info.dart';
+import 'package:highlights/domain/highlights/value_objects.dart';
+
+part 'highlight.freezed.dart';
+
+/// Data class representing information about Highlight entity.
+///
+/// [_$Highlight] is being implemented instead of being used as a mixin.
+/// This allow us to implement a custom `failureOption` getter to validate
+/// all the entity at once. Also, a `failureOrUnit` has been added to
+/// [ValueObject] as a helper to get only failures bypassing type checking.
+/// The relevant use case that justifies this addition is when we need to
+/// validate and give UI feedback for all the entity, for example, when the UI
+/// is not currently showing detailed information about the entity. While user
+/// is, for example, editing a Highlight, we can show her specific failures
+/// using the Value Objects validations.
+@freezed
+abstract class Highlight implements _$Highlight {
+  const Highlight._();
+
+  const factory Highlight({
+    @required UniqueId id,
+    @required HighlightQuote quote,
+    @required HighlightColor color,
+    @required HighlightImage image,
+    @required QuoteInfo info,
+  }) = _Highlights;
+
+  factory Highlight.empty() => Highlight(
+        id: UniqueId(),
+        quote: HighlightQuote(''),
+        color: HighlightColor(HighlightColor.predefinedColors[0]),
+        image: HighlightImage(),
+        info: QuoteInfo.empty(),
+      );
+
+  Option<ValueFailure<dynamic>> get failureOption {
+    return quote.failureOrUnit
+        .andThen(info.failureOption.fold(
+          // Getting the failureOption from the QuoteInfo ENTITY,
+          // NOT a failureOrUnit from a VALUE OBJECT.
+          //
+          // "right(unit)" simulates the Right of failureOrUnit, while
+          // "left(f)" represents the actual failure folded in the
+          // next operation
+          () => right(unit),
+          (f) => left(f),
+        ))
+        .fold((f) => some(f), (_) => none());
+  }
+}
---
+++ lib/domain/highlights/highlight.freezed.dart
@@ -0,0 +1,228 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+// ignore_for_file: deprecated_member_use, deprecated_member_use_from_same_package, use_function_type_syntax_for_parameters, unnecessary_const, avoid_init_to_null, invalid_override_different_default_values_named, prefer_expression_function_bodies
+
+part of 'highlight.dart';
+
+// **************************************************************************
+// FreezedGenerator
+// **************************************************************************
+
+T _$identity<T>(T value) => value;
+
+/// @nodoc
+class _$HighlightTearOff {
+  const _$HighlightTearOff();
+
+// ignore: unused_element
+  _Highlights call(
+      {@required UniqueId id,
+      @required HighlightQuote quote,
+      @required HighlightColor color,
+      @required HighlightImage image,
+      @required QuoteInfo info}) {
+    return _Highlights(
+      id: id,
+      quote: quote,
+      color: color,
+      image: image,
+      info: info,
+    );
+  }
+}
+
+/// @nodoc
+// ignore: unused_element
+const $Highlight = _$HighlightTearOff();
+
+/// @nodoc
+mixin _$Highlight {
+  UniqueId get id;
+  HighlightQuote get quote;
+  HighlightColor get color;
+  HighlightImage get image;
+  QuoteInfo get info;
+
+  @JsonKey(ignore: true)
+  $HighlightCopyWith<Highlight> get copyWith;
+}
+
+/// @nodoc
+abstract class $HighlightCopyWith<$Res> {
+  factory $HighlightCopyWith(Highlight value, $Res Function(Highlight) then) =
+      _$HighlightCopyWithImpl<$Res>;
+  $Res call(
+      {UniqueId id,
+      HighlightQuote quote,
+      HighlightColor color,
+      HighlightImage image,
+      QuoteInfo info});
+
+  $QuoteInfoCopyWith<$Res> get info;
+}
+
+/// @nodoc
+class _$HighlightCopyWithImpl<$Res> implements $HighlightCopyWith<$Res> {
+  _$HighlightCopyWithImpl(this._value, this._then);
+
+  final Highlight _value;
+  // ignore: unused_field
+  final $Res Function(Highlight) _then;
+
+  @override
+  $Res call({
+    Object id = freezed,
+    Object quote = freezed,
+    Object color = freezed,
+    Object image = freezed,
+    Object info = freezed,
+  }) {
+    return _then(_value.copyWith(
+      id: id == freezed ? _value.id : id as UniqueId,
+      quote: quote == freezed ? _value.quote : quote as HighlightQuote,
+      color: color == freezed ? _value.color : color as HighlightColor,
+      image: image == freezed ? _value.image : image as HighlightImage,
+      info: info == freezed ? _value.info : info as QuoteInfo,
+    ));
+  }
+
+  @override
+  $QuoteInfoCopyWith<$Res> get info {
+    if (_value.info == null) {
+      return null;
+    }
+    return $QuoteInfoCopyWith<$Res>(_value.info, (value) {
+      return _then(_value.copyWith(info: value));
+    });
+  }
+}
+
+/// @nodoc
+abstract class _$HighlightsCopyWith<$Res> implements $HighlightCopyWith<$Res> {
+  factory _$HighlightsCopyWith(
+          _Highlights value, $Res Function(_Highlights) then) =
+      __$HighlightsCopyWithImpl<$Res>;
+  @override
+  $Res call(
+      {UniqueId id,
+      HighlightQuote quote,
+      HighlightColor color,
+      HighlightImage image,
+      QuoteInfo info});
+
+  @override
+  $QuoteInfoCopyWith<$Res> get info;
+}
+
+/// @nodoc
+class __$HighlightsCopyWithImpl<$Res> extends _$HighlightCopyWithImpl<$Res>
+    implements _$HighlightsCopyWith<$Res> {
+  __$HighlightsCopyWithImpl(
+      _Highlights _value, $Res Function(_Highlights) _then)
+      : super(_value, (v) => _then(v as _Highlights));
+
+  @override
+  _Highlights get _value => super._value as _Highlights;
+
+  @override
+  $Res call({
+    Object id = freezed,
+    Object quote = freezed,
+    Object color = freezed,
+    Object image = freezed,
+    Object info = freezed,
+  }) {
+    return _then(_Highlights(
+      id: id == freezed ? _value.id : id as UniqueId,
+      quote: quote == freezed ? _value.quote : quote as HighlightQuote,
+      color: color == freezed ? _value.color : color as HighlightColor,
+      image: image == freezed ? _value.image : image as HighlightImage,
+      info: info == freezed ? _value.info : info as QuoteInfo,
+    ));
+  }
+}
+
+/// @nodoc
+class _$_Highlights extends _Highlights {
+  const _$_Highlights(
+      {@required this.id,
+      @required this.quote,
+      @required this.color,
+      @required this.image,
+      @required this.info})
+      : assert(id != null),
+        assert(quote != null),
+        assert(color != null),
+        assert(image != null),
+        assert(info != null),
+        super._();
+
+  @override
+  final UniqueId id;
+  @override
+  final HighlightQuote quote;
+  @override
+  final HighlightColor color;
+  @override
+  final HighlightImage image;
+  @override
+  final QuoteInfo info;
+
+  @override
+  String toString() {
+    return 'Highlight(id: $id, quote: $quote, color: $color, image: $image, info: $info)';
+  }
+
+  @override
+  bool operator ==(dynamic other) {
+    return identical(this, other) ||
+        (other is _Highlights &&
+            (identical(other.id, id) ||
+                const DeepCollectionEquality().equals(other.id, id)) &&
+            (identical(other.quote, quote) ||
+                const DeepCollectionEquality().equals(other.quote, quote)) &&
+            (identical(other.color, color) ||
+                const DeepCollectionEquality().equals(other.color, color)) &&
+            (identical(other.image, image) ||
+                const DeepCollectionEquality().equals(other.image, image)) &&
+            (identical(other.info, info) ||
+                const DeepCollectionEquality().equals(other.info, info)));
+  }
+
+  @override
+  int get hashCode =>
+      runtimeType.hashCode ^
+      const DeepCollectionEquality().hash(id) ^
+      const DeepCollectionEquality().hash(quote) ^
+      const DeepCollectionEquality().hash(color) ^
+      const DeepCollectionEquality().hash(image) ^
+      const DeepCollectionEquality().hash(info);
+
+  @JsonKey(ignore: true)
+  @override
+  _$HighlightsCopyWith<_Highlights> get copyWith =>
+      __$HighlightsCopyWithImpl<_Highlights>(this, _$identity);
+}
+
+abstract class _Highlights extends Highlight {
+  const _Highlights._() : super._();
+  const factory _Highlights(
+      {@required UniqueId id,
+      @required HighlightQuote quote,
+      @required HighlightColor color,
+      @required HighlightImage image,
+      @required QuoteInfo info}) = _$_Highlights;
+
+  @override
+  UniqueId get id;
+  @override
+  HighlightQuote get quote;
+  @override
+  HighlightColor get color;
+  @override
+  HighlightImage get image;
+  @override
+  QuoteInfo get info;
+  @override
+  @JsonKey(ignore: true)
+  _$HighlightsCopyWith<_Highlights> get copyWith;
+}
