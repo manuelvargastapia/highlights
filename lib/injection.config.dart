d70b5b7b0c31dc37de34f86ec6f2a2851f9479b0
manuelvargastapia <manuelvargastapia@gmail.com>
2021-02-12 19:21:50 -0300

    update injections

--- lib/injection.config.dart
+++ lib/injection.config.dart
@@ -5,6 +5,7 @@
 // **************************************************************************
 
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
 import 'package:google_sign_in/google_sign_in.dart';
@@ -14,6 +15,7 @@ import 'infrastructure/authentication/auth_provider_manager.dart';
 import 'infrastructure/authentication/firebase_auth_facade.dart';
 import 'infrastructure/core/firebase_injectable_module.dart';
 import 'application/highlight/highlight_actor/highlight_actor_bloc.dart';
+import 'infrastructure/highlight/highlight_repository.dart';
 import 'application/highlight/highlight_watcher/highlight_watcher_bloc.dart';
 import 'domain/authentication/i_auth_facade.dart';
 import 'domain/highlights/i_highlight_repository.dart';
@@ -32,17 +34,19 @@ GetIt $initGetIt(
   gh.lazySingleton<AuthProviderManager>(() => AuthProviderManager());
   gh.lazySingleton<FirebaseAuth>(() => firebaseInjectableModule.firebaseAuth);
   gh.lazySingleton<GoogleSignIn>(() => firebaseInjectableModule.googleSignIn);
-  gh.factory<HighlightActorBloc>(
-      () => HighlightActorBloc(get<IHighlightRepository>()));
-  gh.factory<HighlightWatcherBloc>(
-      () => HighlightWatcherBloc(get<IHighlightRepository>()));
   gh.lazySingleton<IAuthFacade>(() => FirebaseAuthFacade(
         get<FirebaseAuth>(),
         get<GoogleSignIn>(),
         get<AuthProviderManager>(),
       ));
+  gh.lazySingleton<IHighlightRepository>(
+      () => HighlightRepository(get<FirebaseFirestore>()));
   gh.factory<SignInFormBloc>(() => SignInFormBloc(get<IAuthFacade>()));
   gh.factory<AuthBloc>(() => AuthBloc(get<IAuthFacade>()));
+  gh.factory<HighlightActorBloc>(
+      () => HighlightActorBloc(get<IHighlightRepository>()));
+  gh.factory<HighlightWatcherBloc>(
+      () => HighlightWatcherBloc(get<IHighlightRepository>()));
   return get;
 }
 
