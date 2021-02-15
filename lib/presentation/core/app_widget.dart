4e8fc62cc985cd963daea0a7c8d16f8f4b738fe5
manuelvargastapia <manuelvargastapia@gmail.com>
2021-02-15 18:37:43 -0300

    update theme

--- lib/presentation/core/app_widget.dart
+++ lib/presentation/core/app_widget.dart
@@ -24,14 +24,16 @@ class AppWidget extends StatelessWidget {
         builder: ExtendedNavigator.builder<Router>(router: Router()),
         debugShowCheckedModeBanner: false,
         theme: ThemeData.light().copyWith(
-          primaryColor: Colors.cyan[800],
-          accentColor: Colors.pinkAccent,
-          inputDecorationTheme: InputDecorationTheme(
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(8),
+            primaryColor: Colors.cyan[800],
+            accentColor: Colors.pinkAccent,
+            inputDecorationTheme: InputDecorationTheme(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
             ),
-          ),
-        ),
+            floatingActionButtonTheme: FloatingActionButtonThemeData(
+              backgroundColor: Colors.blue[900],
+            )),
       ),
     );
   }
