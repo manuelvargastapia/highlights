06d49f74f4d14af68e71f0e48260f43ae2d5494a
manuelvargastapia <manuelvargastapia@gmail.com>
2021-02-28 16:12:20 -0300

    WIP: create searchBar

---
+++ lib/presentation/highlight/highlight_overview/widgets/search_bar.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SearchBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 8),
+      child: InkResponse(
+        onTap: () {
+          // TODO: implement
+        },
+        child: const Icon(Icons.search),
+      ),
+    );
+  }
+}
