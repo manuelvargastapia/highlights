fc1cef3135da42f9c46322200bfc78ff0059e56a
manuelvargastapia <manuelvargastapia@gmail.com>
2021-03-01 11:00:12 -0300

    add upload image case to HighlightActorBloc

--- lib/application/highlight/highlight_actor/highlight_actor_event.dart
+++ lib/application/highlight/highlight_actor/highlight_actor_event.dart
@@ -3,4 +3,6 @@ part of 'highlight_actor_bloc.dart';
 @freezed
 abstract class HighlightActorEvent with _$HighlightActorEvent {
   const factory HighlightActorEvent.deleted(Highlight highlight) = _Deleted;
+  const factory HighlightActorEvent.imageUploaded(ImagePrimitive image) =
+      _ImageUploaded;
 }
