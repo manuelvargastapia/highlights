5067cee64ccb0403a1d2b9d805e9c9429f711f38
manuelvargastapia <manuelvargastapia@gmail.com>
2021-03-05 20:12:37 -0300

    create TextRecognitionResult entity

---
+++ lib/domain/text_recognition/text_recognition_result.dart
@@ -0,0 +1,32 @@
+import 'package:dartz/dartz.dart';
+import 'package:highlights/domain/core/failures.dart';
+import 'package:kt_dart/collection.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+import 'package:highlights/domain/text_recognition/value_objects.dart';
+
+part 'text_recognition_result.freezed.dart';
+
+@freezed
+abstract class TextRecognitionResult with _$TextRecognitionResult {
+  const TextRecognitionResult._();
+
+  const factory TextRecognitionResult({
+    @required RecognizedText recognizedText,
+    @required ImmutableList<RecognizedTextElement> recognizedElements,
+    @required Option<ImageSize> imageSize,
+  }) = _TextRecognitionResult;
+
+  factory TextRecognitionResult.empty() => TextRecognitionResult(
+        recognizedText: RecognizedText(''),
+        recognizedElements: ImmutableList(const KtList.empty()),
+        imageSize: none(),
+      );
+
+  Option<ValueFailure<dynamic>> get failureOption {
+    return recognizedText.failureOrUnit.fold(
+      (f) => some(f),
+      (_) => none(),
+    );
+  }
+}
