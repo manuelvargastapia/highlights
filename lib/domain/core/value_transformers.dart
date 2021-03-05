b08fc170a65e820f7bcaa9eaa4499bbacd0d950c
manuelvargastapia <manuelvargastapia@gmail.com>
2021-03-05 20:11:28 -0300

    add trimToMaxLength

--- lib/domain/core/value_transformers.dart
+++ lib/domain/core/value_transformers.dart
@@ -3,3 +3,7 @@ import 'dart:ui';
 Color makeColorOpaque(Color color) {
   return color.withOpacity(1);
 }
+
+String trimToMaxLength(String text, int maxLength) {
+  return text.substring(0, maxLength);
+}
