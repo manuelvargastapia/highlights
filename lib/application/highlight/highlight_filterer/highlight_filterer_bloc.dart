ec8a335c9ca0741a0be15611179b881c52570d87
manuelvargastapia <manuelvargastapia@gmail.com>
2021-03-08 15:18:00 -0300

    handle new filters cases

--- lib/application/highlight/highlight_filterer/highlight_filterer_bloc.dart
+++ lib/application/highlight/highlight_filterer/highlight_filterer_bloc.dart
@@ -1,9 +1,12 @@
 import 'dart:async';
+import 'dart:ui';
 
+import 'package:dartz/dartz.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:injectable/injectable.dart';
 
+import 'package:highlights/domain/highlights/value_objects.dart';
 import 'package:highlights/domain/highlights/highlight_search_filter.dart';
 
 part 'highlight_filterer_event.dart';
@@ -24,6 +27,33 @@ class HighlightFiltererBloc
         yield state.copyWith(
           filters: state.filters.copyWith(
             showOnlyIfHasImage: !state.filters.showOnlyIfHasImage,
+          ),
+        );
+      },
+      // TODO: test
+      filterByColorMatch: (event) async* {
+        yield state.copyWith(
+          filters: state.filters.copyWith(
+            colorMatchOption: event.colorOption.fold(
+              () => none(),
+              (c) => some(HighlightColor(c)),
+            ),
+          ),
+        );
+      },
+      // TODO: test
+      orderByOptionChanged: (event) async* {
+        yield state.copyWith(
+          filters: state.filters.copyWith(
+            orderByOption: event.option,
+          ),
+        );
+      },
+      // TODO: test
+      descendingOrderToggled: (event) async* {
+        yield state.copyWith(
+          filters: state.filters.copyWith(
+            descendingOrder: !state.filters.descendingOrder,
           ),
         );
       },
