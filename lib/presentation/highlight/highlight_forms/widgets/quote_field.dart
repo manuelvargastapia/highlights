ba854ee3edc4683dc61abb8905cb6f6081f303a2
manuelvargastapia <manuelvargastapia@gmail.com>
2021-03-19 21:25:06 -0300

    update QuoteField design

--- lib/presentation/highlight/highlight_forms/widgets/quote_field.dart
+++ lib/presentation/highlight/highlight_forms/widgets/quote_field.dart
@@ -25,44 +25,70 @@ class QuoteField extends HookWidget {
         textEditingController.text = state.highlight.quote.getOrCrash();
       },
       child: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: TextFormField(
-          controller: textEditingController,
-          decoration: const InputDecoration(
-            labelText: 'Quote',
-            counterText: '', // Don't show counter text
-          ),
-          maxLength: HighlightQuote.maxLength,
-          maxLines: null, // Remove restriction to expand dynamically
-          minLines: 5,
-          onChanged: (value) {
-            context
-                .read<HighlightFormBloc>()
-                .add(HighlightFormEvent.quoteChange(value));
-          },
-          autovalidateMode: AutovalidateMode.onUserInteraction,
-
-          // Use state directly from bloc because latter is delayed
-          // (it'll validate the previous state instead of the current
-          // text inputed)
-          validator: (_) => context
-              .read<HighlightFormBloc>()
-              .state
-              .highlight
-              .quote
-              .value
-              .fold(
-                (failure) => failure.maybeMap(
-                  empty: (_) => 'Cannot be empty',
-                  // We don't want to forget this case, but it won't happen
-                  // if we set maxLength: HighlightQuote.maxLength
-                  exceedingLength: (f) => 'Exceeding length. Max: ${f.max}',
-                  orElse: () => null,
+        padding: const EdgeInsets.symmetric(vertical: 16, horizontal: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(bottom: 16),
+              child: Text(
+                'Quote',
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Theme.of(context).backgroundColor,
                 ),
-                // If validator returns null, then any feedback is
-                // provided to user because everithing is right
-                (_) => null,
               ),
+            ),
+            TextFormField(
+              controller: textEditingController,
+              decoration: InputDecoration(
+                floatingLabelBehavior: FloatingLabelBehavior.never,
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16),
+                  borderSide: BorderSide(
+                    color: Theme.of(context).backgroundColor,
+                  ),
+                ),
+                filled: true,
+                fillColor: Colors.white,
+                contentPadding: const EdgeInsets.all(16),
+              ),
+              maxLength: HighlightQuote.maxLength,
+              maxLines: 10,
+              minLines: 10,
+              onChanged: (value) {
+                context
+                    .read<HighlightFormBloc>()
+                    .add(HighlightFormEvent.quoteChange(value));
+              },
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+
+              // Use state directly from bloc because latter is delayed
+              // (it'll validate the previous state instead of the current
+              // text inputed)
+              validator: (_) => context
+                  .read<HighlightFormBloc>()
+                  .state
+                  .highlight
+                  .quote
+                  .value
+                  .fold(
+                    (failure) => failure.maybeMap(
+                      empty: (_) => 'Required',
+                      // We don't want to forget this case, but it won't happen
+                      // if we set maxLength: HighlightQuote.maxLength
+                      exceedingLength: (f) => 'Exceeding length. Max: ${f.max}',
+                      orElse: () => null,
+                    ),
+                    // If validator returns null, then any feedback is
+                    // provided to user because everithing is right
+                    (_) => null,
+                  ),
+            ),
+          ],
         ),
       ),
     );
