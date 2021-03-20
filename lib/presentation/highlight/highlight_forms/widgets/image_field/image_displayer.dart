34e7fc0bf855701631492b007e8c7d46a9d5d2fe
manuelvargastapia <manuelvargastapia@gmail.com>
2021-03-20 15:47:52 -0300

    improve ImageDisplayer design

--- lib/presentation/highlight/highlight_forms/widgets/image_field/image_displayer.dart
+++ lib/presentation/highlight/highlight_forms/widgets/image_field/image_displayer.dart
@@ -1,59 +1,58 @@
-import 'package:dartz/dartz.dart';
-
 import 'package:flutter/material.dart';
 
 import 'package:animations/animations.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 import 'package:highlights/domain/core/errors.dart';
 import 'package:highlights/domain/highlights/image.dart' as d;
 
 class ImageDisplayer extends StatelessWidget {
-  final Option<d.Image> _image;
-  final Size _size;
+  final d.Image _image;
 
-  const ImageDisplayer(this._image, this._size);
+  const ImageDisplayer(this._image);
 
   @override
   Widget build(BuildContext context) {
-    return _image.fold(
-      () => _sizedBoxWrap(const Center(child: Text('NO IMAGE'))),
-      (image) {
-        // TODO: cache image
-        final loadedImage = _loadImage(image);
+    // TODO: cache image
+    final loadedImage = _loadImage(_image);
 
-        return image.failureOption.fold(
-          () => OpenContainer(
-            closedElevation: 8,
-            transitionType: ContainerTransitionType.fadeThrough,
-            transitionDuration: const Duration(milliseconds: 600),
-            closedBuilder: (context, _) => _sizedBoxWrap(loadedImage),
-            openBuilder: (context, _) => loadedImage,
-          ),
-          (failure) => _sizedBoxWrap(
-            Center(
-              child: Text(
-                failure.maybeMap(
-                  invalidUrl: (_) =>
-                      'Error loading image. Try again later, please.',
-                  orElse: () => 'Unexpect error. Try again later, please.',
-                ),
-                maxLines: 3,
-                overflow: TextOverflow.ellipsis,
-                textAlign: TextAlign.center,
+    return _image.failureOption.fold(
+      () => OpenContainer(
+        closedElevation: 4,
+        closedShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        transitionType: ContainerTransitionType.fadeThrough,
+        transitionDuration: const Duration(milliseconds: 600),
+        closedBuilder: (context, _) => _loadedImageContainer(loadedImage),
+        openBuilder: (context, _) => loadedImage,
+      ),
+      (failure) => Material(
+        elevation: 4,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16),
+        ),
+        child: SizedBox(
+          height: 200,
+          child: Center(
+            child: Text(
+              failure.maybeMap(
+                invalidUrl: (_) =>
+                    'Error loading image. Try again later, please.',
+                orElse: () => 'Unexpect error. Try again later, please.',
               ),
+              maxLines: 3,
+              overflow: TextOverflow.ellipsis,
+              textAlign: TextAlign.center,
             ),
           ),
-        );
-      },
+        ),
+      ),
     );
   }
 
-  Widget _sizedBoxWrap(Widget content) {
-    return SizedBox(
-      width: _size.width * 0.6,
-      height: _size.height * 0.2,
-      child: content,
-    );
+  Widget _loadedImageContainer(Image image) {
+    return Center(child: image);
   }
 
   Image _loadImage(d.Image domainImage) {
@@ -68,7 +67,14 @@ class ImageDisplayer extends StatelessWidget {
             ? child
             : AnimatedSwitcher(
                 duration: const Duration(milliseconds: 500),
-                child: frame != null ? child : const Text('Loading...'),
+                child: frame != null
+                    ? child
+                    : SizedBox(
+                        height: 200,
+                        child: SpinKitDoubleBounce(
+                          color: Theme.of(context).accentColor,
+                        ),
+                      ),
               );
 
     Widget errorBuilder(
@@ -76,7 +82,11 @@ class ImageDisplayer extends StatelessWidget {
       Object error,
       StackTrace stackTrace,
     ) =>
-        const Center(child: Text('Error loading image.'));
+        // TODO: show funny image
+        const SizedBox(
+          height: 200,
+          child: Center(child: Text('Error loading image.')),
+        );
 
     return domainImage.isUploaded
         ? Image.network(
