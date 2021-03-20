07d282487b27edadcffe00129a3459f37d4ba01b
manuelvargastapia <manuelvargastapia@gmail.com>
2021-03-19 21:24:21 -0300

    update HasImageChip design

--- lib/presentation/highlight/highlight_overview/widgets/filters_dialog/has_image_chip.dart
+++ lib/presentation/highlight/highlight_overview/widgets/filters_dialog/has_image_chip.dart
@@ -13,14 +13,21 @@ class HasImageChip extends StatelessWidget {
       builder: (context, state) {
         return FilterChip(
           selected: state.filters.showOnlyIfHasImage,
-          label: const Text('Only with Image'),
+          label: const Text('With Image'),
           labelStyle: const TextStyle(
             color: Colors.white,
             fontWeight: FontWeight.bold,
           ),
+          avatar: const Padding(
+            padding: EdgeInsets.only(left: 4),
+            child: Icon(
+              Icons.image,
+              color: Colors.white,
+            ),
+          ),
           backgroundColor: Theme.of(context).backgroundColor,
           selectedColor: Theme.of(context).accentColor,
-          checkmarkColor: Colors.white,
+          showCheckmark: false,
           onSelected: (_) {
             context
                 .read<HighlightFiltererBloc>()
