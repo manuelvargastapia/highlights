db2890ad94c4b3436045d67b9e216475face9b30
manuelvargastapia <manuelvargastapia@gmail.com>
2021-04-28 19:55:41 -0400

    show critical error and tahnks feedback after sending report

--- lib/presentation/highlight/highlight_overview/widgets/critical_failure_display.dart
+++ lib/presentation/highlight/highlight_overview/widgets/critical_failure_display.dart
@@ -17,47 +17,43 @@ class CriticalFailureDisplay extends HookWidget {
   Widget build(BuildContext context) {
     final emailSended = useState(false);
 
-    return Center(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Text(
-            emailSended.value ? '😸' : '😱',
-            style: const TextStyle(fontSize: 100),
-          ),
-          Text(
-            emailSended.value
-                ? 'Thanks for your report'
-                : failure.maybeMap(
-                    insufficientPermission: (_) => 'Insufficient permissions',
-                    orElse: () => 'Unexpected error.\nPlease, contact support.',
-                  ),
-            textAlign: TextAlign.center,
-            style: const TextStyle(fontSize: 24),
-          ),
-          if (!emailSended.value)
-            TextButton(
-              onPressed: () async {
-                final Email email = Email(
-                  body: 'Technical details:\n\n ${failure.toString()}',
-                  subject: 'Highlights: Critical error report',
-                  recipients: ['code@manuelvargas.dev'],
-                );
-
-                await FlutterEmailSender.send(email);
-
-                emailSended.value = true;
-              },
-              child: Row(
-                mainAxisSize: MainAxisSize.min,
-                children: const [
-                  Icon(Icons.mail),
-                  SizedBox(width: 4),
-                  Text('REPORT ERROR'),
-                ],
+    return SingleChildScrollView(
+      child: Padding(
+        padding: EdgeInsets.only(top: emailSended.value ? 48 : 0),
+        child: Column(
+          children: [
+            if (emailSended.value)
+              const Image(image: AssetImage('assets/thank_you_report.png')),
+            if (!emailSended.value) ...[
+              const Image(image: AssetImage('assets/critical_failure.png')),
+              const Text(
+                'An error has ocurred! Please, consider sending a report.',
+                style: TextStyle(fontSize: 20),
+                textAlign: TextAlign.center,
               ),
-            ),
-        ],
+              const SizedBox(height: 16),
+              TextButton(
+                onPressed: () async {
+                  final Email email = Email(
+                    body: 'Technical details:\n\n ${failure.toString()}',
+                    subject: 'Highlights: Critical error report',
+                    recipients: ['code@manuelvargas.dev'],
+                  );
+                  await FlutterEmailSender.send(email);
+                  emailSended.value = true;
+                },
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: const [
+                    Icon(Icons.mail),
+                    SizedBox(width: 4),
+                    Text('REPORT ERROR'),
+                  ],
+                ),
+              ),
+            ]
+          ],
+        ),
       ),
     );
   }
